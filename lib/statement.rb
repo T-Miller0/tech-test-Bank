0896bb82ee936c515b8022968f5140d1f08233ac
tom miller <thomas.j.c.miller@outlook.com>
2018-11-05 15:25:06 +0000

    deposit_statement pushed into account_statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,18 @@
+require_relative "the_date"
+require_relative "account_management"
 # Statment to show date, amount, balance
 class Statement
   attr_reader :account_statement
 
   def initialize
     @account_statement = []
+  end
+
+  def deposit_statement (new_date, top_up, balance)
+    @account_statement.push ({
+      Date: new_date,
+      Credit_Debit: top_up,
+      Balance: balance
+    })
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,4 +4,8 @@ describe Statement do
   it 'Return an emply statement' do
     expect(subject.account_statement).to eq []
   end
+
+  it 'Deposit shows date, amount and total balance' do
+    expect(subject.deposit_statement('10/01/2012', 1000, 1000)).to eq [{Date: "10/01/2012", Credit_Debit: 1000, Balance: 1000}]
+  end
 end
