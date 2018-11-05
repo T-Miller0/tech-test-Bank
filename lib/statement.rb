085366851a49a325e6ce78e69c330501b419d88c
tom miller <thomas.j.c.miller@outlook.com>
2018-11-05 16:43:48 +0000

    corrected balance to be auto generated

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,17 +2,18 @@ require_relative "the_date"
 require_relative "account_management"
 # Statment to show date, amount, balance
 class Statement
-  attr_reader :account_statement
+  attr_reader :account_statement, :account_management
 
-  def initialize
+  def initialize (account_management = AccountManagement.new)
     @account_statement = []
+    @account_management = account_management
   end
 
-  def deposit_statement (new_date, top_up, balance)
+  def deposit_statement (new_date, amount)
     @account_statement.push ({
       Date: new_date,
-      Credit_Debit: top_up,
-      Balance: balance
+      Credit_Debit: @account_management.top_up(amount),
+      Balance: account_management.balance
     })
   end
 end
