ae4599ce88dbfe387daebd9ad4ab0bd80322a44c
tom miller <thomas.j.c.miller@outlook.com>
2018-11-05 15:47:03 +0000

    deposit_statement is added to account_statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -8,4 +8,9 @@ describe Statement do
   it 'Deposit shows date, amount and total balance' do
     expect(subject.deposit_statement('10/01/2012', 1000, 1000)).to eq [{Date: "10/01/2012", Credit_Debit: 1000, Balance: 1000}]
   end
+
+  it 'After a deposit account_statement is not empty' do
+    subject.deposit_statement('10/01/2012', 1000, 1000)
+    expect(subject.account_statement).not_to be_empty
+  end
 end
