5aa9ba8964198d4877cd07fb1902f0f51ab0b4cf
tom miller <thomas.j.c.miller@outlook.com>
2018-11-05 11:10:46 +0000

    top_up method added

--- lib/statement.rb
+++ lib/statement.rb
@@ -5,4 +5,8 @@ class Statement
   def initialize
     @balance = 0
   end
+
+  def top_up(amount)
+    @balance += amount
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,4 +4,8 @@ describe Statement do
   it 'User has a balance' do
     expect(subject.balance).to eq 0
   end
+
+  it "User can #top_up" do
+    expect(subject.top_up(100)).to eq 100
+  end
 end
