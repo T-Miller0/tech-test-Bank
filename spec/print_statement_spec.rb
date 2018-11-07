61bc1e5986b1e5ca3741e2a517e54791c06965e4
tom miller <thomas.j.c.miller@outlook.com>
2018-11-07 12:32:17 +0000

    test now mocks dependancy

--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -2,20 +2,19 @@ require 'print_statement'
 
 describe PrintStatement do
 
-  statement = Statement.new
-  my_acc = statement.account_statement
-
   it "Prints an empty user statement" do
+    my_acc = []
     expect {subject.my_statement(my_acc)}.to output("date || credit || debit || balance\n").to_stdout
   end
 
   it "Prints a deposit_statement" do
-    statement.deposit_statement('10/01/2012', 1000.00)
+    my_acc = [{
+      Date: '10/01/2012',
+      Credit: 1000.00,
+      Balance: 1000.00
+    }]
+
     expect {subject.my_statement(my_acc)}.to output("date || credit || debit || balance\n"\
     "10/01/2012 || 1000.00 ||  || 1000.00\n").to_stdout
-  end
-
-  it 'format_num returns a number to two decimals' do
-    expect(subject.format_num(10.99)).to eq ("10.99")
   end
 end
