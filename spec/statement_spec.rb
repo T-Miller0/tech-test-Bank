41ca5bd4bb2d7f136523933050b8a5b13d8253b0
tom miller <thomas.j.c.miller@outlook.com>
2018-11-07 12:31:50 +0000

    changed tests to mock dependancy

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,13 +1,13 @@
 require 'statement'
 
 describe Statement do
-  it 'Return an emply statement' do
+  it 'Return an emptly statement' do
     expect(subject.account_statement).to eq []
   end
 
   it 'Deposit shows date, amount and total balance' do
     subject.deposit_statement('10/01/2012', 1000)
-    expect(subject.account_statement).to eq [{ Date: '10/01/2012', Credit: 1000, Balance: 1000 }]
+    expect(subject.account_statement).to eq [{ Date: '10/01/2012', Credit: 1000.00, Balance: 1000 }]
   end
 
   it 'After a deposit account_statement is not empty' do
@@ -15,10 +15,28 @@ describe Statement do
     expect(subject.account_statement).not_to be_empty
   end
 
+  it 'shows a #deposit_statement' do
+    acc_management = double(AccountManagement)
+    statement = Statement.new(acc_management)
+
+    expect(acc_management).to receive(:top_up).with(1000)
+    expect(acc_management).to receive(:total_balance)
+
+    statement.deposit_statement('14/01/2012', 1000)
+  end
+
   it 'shows a #withdrawal_statement' do
-    subject.account_management.top_up(1000)
-    subject.withdrawal_statement('14/01/2012', 500)
-    expect(subject.account_statement).to eq [{ Date: '14/01/2012', Debit: -500, Balance: 500 }]
+    acc_management = double(AccountManagement)
+    statement = Statement.new(acc_management)
+
+    allow(acc_management).to receive(:top_up).with(1000)
+    allow(acc_management).to receive(:total_balance)
+
+    statement.deposit_statement('14/01/2012', 1000)
+
+    expect(acc_management).to receive(:withdraw).with(500)
+
+    statement.withdrawal_statement('14/01/2012', 500)
   end
 
   it '#account_statement has deposit_statement and withdrawal_statement' do
